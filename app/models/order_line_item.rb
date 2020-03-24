1c4703a38bbec351585e97206eaf313cbac36404
Asher Wright <sastreen@hotmail.com>
2020-03-24 15:24:48 -0400

    Fix incorrect relationship

--- app/models/order_line_item.rb
+++ app/models/order_line_item.rb
@@ -1,5 +1,5 @@
 class OrderLineItem < ApplicationRecord
-    has_one :grocery_item
+    belongs_to :grocery_item
     belongs_to :order
     validates :grocery_item, presence: true
     validates :order, presence: true
--- db/migrate/20200324190842_create_order_line_items.rb
+++ db/migrate/20200324190842_create_order_line_items.rb
@@ -2,7 +2,7 @@ class CreateOrderLineItems < ActiveRecord::Migration[6.0]
   def change
     create_table :order_line_items do |t|
       t.integer :quantity
-      t.references :grocery_item
+      t.belongs_to :grocery_item
       t.belongs_to :order
       t.timestamps
     end
