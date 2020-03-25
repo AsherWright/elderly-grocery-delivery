14d17a47765bf36fea5292a975381b4c8158f206
Sam Brownstone <sbrownrock@gmail.com>
2020-03-24 21:39:27 -0400

    add input form on add item modal

--- app/javascript/bundles/components/NewItemModal.tsx
+++ app/javascript/bundles/components/NewItemModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Container, Col, Row, Form, ListGroup, Modal} from 'react-bootstrap';
+import {Button, Container, Col, Row, Form, ListGroup, Modal, InputGroup, FormControl} from 'react-bootstrap';
 import {GroceryItem, GroceryList} from '.'
 
 interface NewItemModalProps {
@@ -16,7 +16,7 @@ export default class NewItemModal extends React.Component<NewItemModalProps> {
 
     <Modal
       show={this.props.isVisible}
-      size="lg"
+      size="md"
       aria-labelledby="contained-modal-title-vcenter"
       centered
       onHide = {this.props.onClose}
@@ -27,12 +27,32 @@ export default class NewItemModal extends React.Component<NewItemModalProps> {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <p>
-          Enter the information below to add a new item to your grocery list. 
-        </p>
-        <Form.Group>
-                  <Form.Control size="lg" type="text" placeholder="Search for the groceries you need"/>
-        </Form.Group>
+        <Row>
+            <Col>
+            <h5>What is the item called?</h5>
+            <InputGroup className="mb-3">
+                <FormControl
+                    placeholder="e.g. Bananas"
+                    aria-label="name"
+                    aria-describedby="inputGroup-sizing-default"
+                  />
+            </InputGroup>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+            <h5>How much are you willing to pay for it?</h5>
+            <InputGroup className='mb-3'>
+                <InputGroup.Prepend>
+                    <InputGroup.Text id="inputGroup-sizing-default">$</InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                    placeholder="e.g. 3.00"
+                    aria-label="price"
+                    />
+            </InputGroup>
+            </Col>
+            </Row>
         </Modal.Body>
         <Modal.Footer>
             <Button onClick={this.props.onAdd} variant='primary'>Add item</Button>
