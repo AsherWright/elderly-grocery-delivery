7b4e03aa1924264be69783654efcfef6e750719e
Asher Wright <sastreen@hotmail.com>
2020-03-26 13:47:39 -0400

    Add fetch request to Order page

--- app/javascript/bundles/Order/components/Order.tsx
+++ app/javascript/bundles/Order/components/Order.tsx
@@ -1,13 +1,67 @@
+import { RouteComponentProps } from 'react-router-dom';
 import React from 'react';
 
-export default class Order extends React.Component {
+interface FetchOrderResponse {
+    id: string;
+    created_at: string;
+}
+
+interface Order {
+    id: string;
+    createdAt: string;
+}
+
+interface OrderState {
+    order: Order;
+}
+
+class OrderPage extends React.Component<RouteComponentProps, OrderState> {
+    constructor(props: RouteComponentProps) {
+        super(props)
+
+        this.state = {
+            order: {
+                id: "",
+                createdAt: ""
+            },
+        }
+    }
+
+    componentDidMount(): void {
+        const orderIdMatches = window.location.pathname.match(/orders\/(.*)$/)
+        const orderId = orderIdMatches && orderIdMatches[1]
+
+        const url = "/api/v1/orders/show/" + orderId;
+        fetch(url)
+            .then((response: Response) => {
+                if (response.ok) {
+                    return response.json() as Promise<FetchOrderResponse>;
+                }
+                throw new Error("Network response was not ok.");
+            })
+            .then(response => this.setState(
+                {
+                    order: {
+                        createdAt: response.created_at,
+                        id: response.id
+                    }
+                }
+            ))
+            .catch(() => this.props.history.push("/"));
+    }
+
     render(): JSX.Element {
         return (
             <div>
                 <h3>
-                    Order goes here
+                    Hi: {this.state.order.id}
+                </h3>
+                <h3>
+                    Hi: {this.state.order.createdAt}
                 </h3>
             </div>
         );
     }
 }
+
+export default OrderPage;
