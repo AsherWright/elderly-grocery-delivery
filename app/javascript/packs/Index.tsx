8aeed46183b8aa4b5e4af862a057ba6c1c28ae02
Asher Wright <sastreen@hotmail.com>
2020-03-26 14:41:54 -0400

    Enable client side rendering

--- app/javascript/packs/Index.tsx
+++ app/javascript/packs/Index.tsx
@@ -1,10 +1,7 @@
-import React from "react";
-import { render } from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from "react";
+import { render } from "react-dom";
 import App from "../components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -13,4 +10,3 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.appendChild(document.createElement("div"))
   );
 });
-
